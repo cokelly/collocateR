ccf6f189c773bebc5ecda44aec4b65fee7048d77
Ciarán O'Kelly <okelly.ciaran@gmail.com>
2016-05-12 18:44:29 +0100

    Added parallization

--- R/Process_MIM_Ngram_Scores.R
+++ R/Process_MIM_Ngram_Scores.R
@@ -5,16 +5,47 @@
 #' @param window The width of the kwic vector
 #' @param ngram The ngram types
 #' @param cutoff A floor for infrequent terms
+#' @param parallel TOkenize using parallel package
 #' @importFrom quanteda corpus kwic is.corpus tokenize ngrams
+#' @import doParallel
+#' @import parallel
 #' @keywords kwic corpus mutual information
 #' @export
 
-pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3){
+pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3, parallel = TRUE){
       # Test to see if the vect is a corpus
       classtest <- class(vect)
       if(is.corpus(vect) == FALSE){vect <- corpus(vect)}
-      # Tokenize with negrams
-      vect_phrases <- tokenize(vect, ngrams = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE)
+      
+      # Tokenize with ngrams
+      
+      # Generic function for parallelizing any task (when possible) from https://rstudio-pubs-static.s3.amazonaws.com/169109_dcd8434e77bb43da8cf057971a010a56.html
+      parallelizeTask <- function(task, ...) {
+            # Calculate the number of cores
+            ncores <- detectCores() - 1
+            # Initiate cluster
+            cl <- makeCluster(ncores)
+            registerDoParallel(cl)
+            #print("Starting task")
+            r <- task(...)
+            #print("Task done")
+            stopCluster(cl)
+            r
+      }
+      
+      # A make tokens function from https://rstudio-pubs-static.s3.amazonaws.com/169109_dcd8434e77bb43da8cf057971a010a56.html
+      makeTokens <- function(input, ngram) {
+            tokenize(input, what = "word", removeNumbers = TRUE,
+                     removePunct = TRUE, removeSeparators = TRUE,
+                     removeTwitter = TRUE, removeHyphens = FALSE,
+                     ngram, simplify = TRUE, concatenator = " ")
+      }
+      if(parallel == TRUE){
+            vect_phrases <- parallelizeTask(makeTokens, vect, ngram)
+      } else {
+      vect_phrases <- tokenize(vect, ngrams = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE, simplify = TRUE)
+      }
+      
       # Create a data frame with ngram frequency for each phrase
       vectdf <- data.frame(table(unlist(vect_phrases)), row.names = NULL) 
       # A column with the probability for each word
@@ -33,7 +64,7 @@ pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3){
       
       ## THERE IS SOMETHING WRONG WITH THE BELOW THAT IS FUCKING EVERYTHING UP.
       
-      overalldf <- merge(kwicdf, vectdf, by = "phrase", all.x = TRUE, all.y = TRUE)
+      overalldf <- merge(vectdf, kwicdf, by = "phrase", all.x = FALSE, all.y = TRUE)
       # retain the keyword probability
       keyword_probs <- overalldf[which(overalldf[,1] == keyword),]
       #remove words and phrases that fall below the cutoff
@@ -51,6 +82,6 @@ pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3){
       # Add to df
       overalldf <- data.frame(cbind(overalldf, pmi))
       #colnames(overalldf) <- c("word/phrase", "doc_freq", "kwic_freq", "pmi")
-      # #overalldf <- overalldf[order(overalldf$pmi, decreasing = TRUE),]
- return(overalldf)
+      overalldf <- overalldf[order(overalldf$pmi, decreasing = TRUE),]
+      return(overalldf)
 }
