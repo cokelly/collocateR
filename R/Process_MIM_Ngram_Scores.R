23c3b2016ef8ef4fb5c1391eff2aa707eafa48ce
Ciarán O'Kelly <okelly.ciaran@gmail.com>
2016-05-13 11:08:12 +0100

    Data table implementation of pmi function

--- R/Process_MIM_Ngram_Scores.R
+++ R/Process_MIM_Ngram_Scores.R
@@ -6,14 +6,16 @@
 #' @param ngram The ngram types
 #' @param cutoff A floor for infrequent terms
 #' @param parallel TOkenize using parallel package
-#' @importFrom quanteda corpus kwic is.corpus tokenize ngrams
+#' @importFrom quanteda corpus kwic is.corpus tokenize ngrams wordstem
 #' @import doParallel
 #' @import parallel
+#' @import data.table
 #' @keywords kwic corpus mutual information
 #' @export
 
 pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3, parallel = TRUE){
       # Test to see if the vect is a corpus
+      
       classtest <- class(vect)
       if(is.corpus(vect) == FALSE){vect <- corpus(vect)}
       
@@ -38,50 +40,52 @@ pmi_score <- function(vect, keyword, window = 5, ngram = 1:4, cutoff = 3, parall
             tokenize(input, what = "word", removeNumbers = TRUE,
                      removePunct = TRUE, removeSeparators = TRUE,
                      removeTwitter = TRUE, removeHyphens = FALSE,
-                     ngram, simplify = TRUE, concatenator = " ")
+                     n = ngram, simplify = TRUE, concatenator = " ")
       }
-      if(parallel == TRUE){
+      if(isTRUE(parallel) == TRUE){
             vect_phrases <- parallelizeTask(makeTokens, vect, ngram)
       } else {
-      vect_phrases <- tokenize(vect, ngrams = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE, simplify = TRUE)
+      vect_phrases <- tokenize(vect, n = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE, simplify = TRUE)
       }
-      
       # Create a data frame with ngram frequency for each phrase
-      vectdf <- data.frame(table(unlist(vect_phrases)), row.names = NULL) 
+      vectdf <- as.data.table(table(unlist(vect_phrases)), row.names = NULL, stringsAsFactors = FALSE) 
       # A column with the probability for each word
-      word_probs <- vectdf[,2]/sum(vectdf[,2])
+      colnames(vectdf) <- c("phrase", "doc_freq")
+      word_probs <- vectdf$doc_freq/sum(vectdf$doc_freq)
       vectdf <- cbind(vectdf, word_probs)
       colnames(vectdf) <- c("phrase", "doc_freq", "word_prob")
+      setkey(vectdf, phrase)
       # Create a similar dataframe for the collocations so I can narrow to relevant words and phrases
       kwic_corp0 <- kwic_to_vect(vect, keywords = keyword, window = window)
       kwic_corp <- corpus(kwic_corp0)
-      kwic_phrases <- tokenize(kwic_corp, ngrams = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE)
-      kwicdf <- data.frame(table(unlist(kwic_phrases)), row.names = NULL)
-      kwic_probs <- kwicdf[,2]/sum(kwicdf[,2])
+      if(isTRUE(parallel) == TRUE){
+            kwic_phrases <- parallelizeTask(makeTokens, input = kwic_corp, n = ngram)
+      } else {
+            vect_phrases <- tokenize(vect, n = ngram, concatenator = " ", removeSymbols = TRUE, removeNumbers = TRUE, simplify = TRUE)}
+      kwicdf <- as.data.table(table(unlist(kwic_phrases)), row.names = NULL, stringsAsFactors = FALSE)
+      colnames(kwicdf) <- c("phrase", "kwic_freq")
+      kwic_probs <- kwicdf$kwic_freq/sum(kwicdf$kwic_freq)
       kwicdf <- cbind(kwicdf, kwic_probs)
       colnames(kwicdf) <- c("phrase", "kwic_freq", "kwic_prob")
+      setkey(kwicdf, phrase)
       # Merge both dfs
-      
-      ## THERE IS SOMETHING WRONG WITH THE BELOW THAT IS FUCKING EVERYTHING UP.
-      
-      overalldf <- merge(vectdf, kwicdf, by = "phrase", all.x = FALSE, all.y = TRUE)
+      overalldf <- vectdf[.(kwicdf)]
       # retain the keyword probability
-      keyword_probs <- overalldf[which(overalldf[,1] == keyword),]
-      #remove words and phrases that fall below the cutoff
+      keyword_probs <- overalldf[which(overalldf$phrase == keyword),]
+      # #remove words and phrases that fall below the cutoff
       floor <- which(overalldf$kwic_freq < cutoff)
       overalldf2 <- overalldf[-floor,]
       # Mutual information measure = log2(prob(xy)/prob(x)prob(y))
       #Calculate prob(xy)
-      kwic_probs_abridged <- overalldf$kwic_prob
+      kwic_probs_abridged <- overalldf2$kwic_freq
       probxy <- kwic_probs_abridged/keyword_probs$kwic_prob
       #Calculate prob(x)prob(y)
-      doc_probs_abridged <- overalldf$word_prob
+      doc_probs_abridged <- overalldf2$word_prob
       probxproby <- doc_probs_abridged*keyword_probs$word_prob
       #calculate mutual information
       pmi <- log2(probxy/probxproby)
-      # Add to df
-      overalldf <- data.frame(cbind(overalldf, pmi))
-      #colnames(overalldf) <- c("word/phrase", "doc_freq", "kwic_freq", "pmi")
-      overalldf <- overalldf[order(overalldf$pmi, decreasing = TRUE),]
-      return(overalldf)
+      # # Add to df
+      overalldf2 <- cbind(overalldf2, pmi)
+      overalldf <- overalldf2[order(overalldf2$pmi, decreasing = TRUE),]
+      return(overalldf2)
 }
