77a91158a303b1740a95c9e8ca3261bc06f51823
Ciarán O'Kelly <okelly.ciaran@gmail.com>
2017-07-04 12:51:59 +0100

    Escape the full stop

--- R/save_collocates.R
+++ R/save_collocates.R
@@ -60,8 +60,8 @@ save_collocates <- function(document, window, node, remove_stops = TRUE, remove_
             document <- str_replace_all(document, "[0-9]", "")
       }
       if(remove_punct == TRUE){
-            document <- gsub(".", " ", document)
-            document <- gsub("'", "", document)
+            document <- gsub("\\.", " ", document)
+            document <- gsub("\\'", "", document)
             document <- str_replace_all(document, "[^[:alnum:]]", " ")
       }
       # To lower
