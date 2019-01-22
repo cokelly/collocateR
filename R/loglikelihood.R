ab1d2eb8c59507840733c90553fbabba770df75e
Ciarán O'Kelly Laptop <okelly.ciaran@gmail.com>
2019-01-22 16:36:53 +0000

    Added explicit function names

--- R/loglikelihood.R
+++ R/loglikelihood.R
@@ -22,20 +22,19 @@ loglikelihood <- function(doc = doc, keyword = keyword, window = 6, ngram = 1, r
       # Extract the total number of words in the kwic 'corpus
       kwic_total_words <- sum(freqs0$kwic_count)
       freqs <- freqs0 %>%
-            filter(kwic_count >= as.numeric(min_count))
-      
-      
+            dplyr::filter(kwic_count >= as.numeric(min_count))
+
       # Calculate the log likelihood score
       loglikelihood <- freqs %>%
-            add_column(wordcount = rep(sum(str_count(doc, "\\S+")), nrow(.))) %>% # Total wordcount
-            mutate(c = as.numeric(kwic_total_words-kwic_count)) %>%
-            mutate(d = as.numeric(wordcount - doc_count)) %>%
-            mutate(E1 = as.numeric((c*(kwic_count+doc_count)/(c+d)))) %>%
-            mutate(E2 = as.numeric((d*(kwic_count+doc_count)/(c+d)))) %>%
-            mutate(loglikelihood = as.numeric((kwic_count*(log(kwic_count/E1)))+(doc_count*log(doc_count/E2)))) %>%
-            select(ngram, loglikelihood) %>%
-            arrange(desc(loglikelihood)) %>%
-            mutate(`p <` = case_when(loglikelihood >= 15.13 ~ "0.0001",
+            tibble::add_column(wordcount = rep(sum(stringr::str_count(doc, "\\S+")), nrow(.))) %>% # Total wordcount
+            dplyr::mutate(c = as.numeric(kwic_total_words-kwic_count)) %>%
+            dplyr::mutate(d = as.numeric(wordcount - doc_count)) %>%
+            dplyr::mutate(E1 = as.numeric((c*(kwic_count+doc_count)/(c+d)))) %>%
+            dplyr::mutate(E2 = as.numeric((d*(kwic_count+doc_count)/(c+d)))) %>%
+            dplyr::mutate(loglikelihood = as.numeric((kwic_count*(log(kwic_count/E1)))+(doc_count*log(doc_count/E2)))) %>%
+            dplyr::select(ngram, loglikelihood) %>%
+            dplyr::arrange(desc(loglikelihood)) %>%
+            dplyr::mutate(`p <` = dplyr::case_when(loglikelihood >= 15.13 ~ "0.0001",
                                             loglikelihood >= 10.83 & loglikelihood < 15.13 ~ "0.001",
                                             loglikelihood >= 6.63 & loglikelihood < 10.83 ~ "0.01",
                                             loglikelihood >= 3.84 & loglikelihood < 6.63 ~ "0.05"))
